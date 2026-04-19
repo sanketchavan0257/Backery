eebc7fde68d86b53049e407431129b9792646057
sanketchavan0257 <sanketchavan0257@gmail.com>
2026-04-19 13:25:39 +0530

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -9,7 +9,7 @@ import { Button } from '../components/ui/button';
 import { Input } from '../components/ui/input';
 import { useAuth } from '../contexts/AuthContext';
 
-const API = `${process.env.REACT_APP_BACKEND_URL}/api`;
+const API = "https://cakeshop-fullstack-project.onrender.com/api";
 //console.log("API:", process.env.REACT_APP_BACKEND_URL);
 
 export default function HomePage() {
