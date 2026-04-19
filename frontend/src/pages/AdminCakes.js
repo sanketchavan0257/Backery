4344a779091bf1562d502108dbcb83df6e14fb7f
sanketchavan0257 <sanketchavan0257@gmail.com>
2026-04-19 13:26:20 +0530

    Update AdminCakes.js

--- frontend/src/pages/AdminCakes.js
+++ frontend/src/pages/AdminCakes.js
@@ -18,7 +18,7 @@ import {
   AlertDialogTitle,
 } from '../components/ui/alert-dialog';
 
-const API = `${process.env.REACT_APP_BACKEND_URL}/api`;
+const API = "https://cakeshop-fullstack-project.onrender.com/api";
 
 export default function AdminCakes() {
   const [cakes, setCakes] = useState([]);
