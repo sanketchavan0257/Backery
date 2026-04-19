fe1cc851efef80a468c6a3e73f8e2f70aee03865
sanketchavan0257 <sanketchavan0257@gmail.com>
2026-04-19 13:17:26 +0530

    Update AuthContext.js

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 const AuthContext = createContext(null);
 
-const API = `${process.env.REACT_APP_BACKEND_URL}/api`;
+//const API = `${process.env.REACT_APP_BACKEND_URL}/api`;
+const API = "https://cakeshop-fullstack-project.onrender.com/api";
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
